f4d2a1e8a843056250dbf986e4ee46001877e596
Munkhbold <munkhbold.d@nmtec.co>
2020-06-18 11:29:07 -0500

    added my order history

--- src/app/services/order.service.ts
+++ src/app/services/order.service.ts
@@ -1,9 +1,25 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 @Injectable()
 export class OrderService {
 
   constructor(private http: HttpClient) {
+  }
+
+  getOrdersByUser() {
+    const token = localStorage.getItem('token');
+    const header = {
+      headers: new HttpHeaders().set('Authorization',  `Basic ${token}`)
+    };
+    return this.http.get<any>('http://localhost:3000/orders', header);
+  }
+
+  cancelOrder(orderId){
+    const token = localStorage.getItem('token');
+    const header = {
+      headers: new HttpHeaders().set('Authorization',  `Basic ${token}`)
+    };
+    return this.http.put<any>(`http://localhost:3000/orders/${orderId}`, {status: 'canceled'}, header);
   }
 }
